474ca6e36fefdb1c2a2ccad6fca7de1eee03eb22
Rafael Ortiz <rafael.ortiz@redcanary.com>
2021-05-26 11:40:15 -0400

    Remove ThreadError

--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,7 @@ use nix::errno::Errno;
 use std::any::Any;
 use std::ffi::NulError;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum OxidebpfError {
     InvalidElf,
     InvalidElfMachine,
@@ -11,5 +11,4 @@ pub enum OxidebpfError {
     PerfEventDoesNotExist,
     PerfIoctlError(nix::Error),
     CStringConversionError(NulError),
-    ThreadError(Box<dyn Any + Send>),
 }
