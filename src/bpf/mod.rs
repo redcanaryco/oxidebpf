2936aed5124bee72219cdf7c0b167f3dd4b1ad2e
Rafael Ortiz <rafael.ortiz@redcanary.com>
2021-05-28 12:14:47 -0400

    get it to build

--- src/bpf/mod.rs
+++ src/bpf/mod.rs
@@ -8,49 +8,49 @@ pub(crate) mod constant;
 pub(crate) mod syscall;
 
 #[repr(C)]
-union PerfSample {
-    sample_period: c_ulong,
-    sample_freq: c_ulong,
+pub(crate) union PerfSample {
+    pub(crate) sample_period: c_ulong,
+    pub(crate) sample_freq: c_ulong,
 }
 
 #[repr(C)]
-union PerfWakeup {
-    wakeup_events: c_uint,
-    wakeup_watermark: c_uint,
+pub(crate) union PerfWakeup {
+    pub(crate) wakeup_events: c_uint,
+    pub(crate) wakeup_watermark: c_uint,
 }
 
 #[repr(C)]
-union PerfBpAddr {
-    bp_addr: c_ulong,
-    config1: c_ulong,
+pub(crate) union PerfBpAddr {
+    pub(crate) bp_addr: c_ulong,
+    pub(crate) config1: c_ulong,
 }
 
 #[repr(C)]
-union PerfBpLen {
-    bp_len: c_ulong,
-    config2: c_ulong,
+pub(crate) union PerfBpLen {
+    pub(crate) bp_len: c_ulong,
+    pub(crate) config2: c_ulong,
 }
 
 #[repr(C)]
-pub(crate) struct PerfEventAttr {
-    p_type: c_uint,
-    size: c_uint,
-    config: c_ulong,
-    sample_union: PerfSample,
-    sample_type: c_ulong,
-    read_format: c_ulong,
-    flags: c_ulong,
-    wakeup_union: PerfWakeup,
-    bp_type: c_uint,
-    bp_addr_union: PerfBpAddr,
-    bp_len_union: PerfBpLen,
-    branch_sample_type: c_ulong, // enum perf_branch_sample_type
-    sample_regs_user: c_ulong,
-    sample_stack_user: c_uint,
-    clockid: c_int,
-    sample_regs_intr: c_ulong,
-    aux_watermark: c_uint,
-    __reserved_2: c_uint, // align to __u64
+pub struct PerfEventAttr {
+    pub(crate) p_type: c_uint,
+    pub(crate) size: c_uint,
+    pub(crate) config: c_ulong,
+    pub(crate) sample_union: PerfSample,
+    pub(crate) sample_type: c_ulong,
+    pub(crate) read_format: c_ulong,
+    pub(crate) flags: c_ulong,
+    pub(crate) wakeup_union: PerfWakeup,
+    pub(crate) bp_type: c_uint,
+    pub(crate) bp_addr_union: PerfBpAddr,
+    pub(crate) bp_len_union: PerfBpLen,
+    pub(crate) branch_sample_type: c_ulong, // enum perf_branch_sample_type
+    pub(crate) sample_regs_user: c_ulong,
+    pub(crate) sample_stack_user: c_uint,
+    pub(crate) clockid: c_int,
+    pub(crate) sample_regs_intr: c_ulong,
+    pub(crate) aux_watermark: c_uint,
+    pub(crate) __reserved_2: c_uint, // align to __u64
 }
 
 #[repr(align(8), C)]
